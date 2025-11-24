7888ad47634c9b36e24a75f385835871b9ea7565
Matheus Mechetti Lulli <matheus.lulli@aluno.cefetmg.br>
2025-11-24 17:58:12 -0300

    Update index.js

--- html_css_js/site_teste/Js/index.js
+++ html_css_js/site_teste/Js/index.js
@@ -1,4 +1,17 @@
 alert('Você está entrando página de Javascript!');
 
 document.getElementById("texto").innerHTML="Meu primeiro texto JS!";
-console.log("oi isso é um console.log")
+console.log("oi isso é um console.log");
+let a,b,c;
+a=4;
+b=1;
+c=3;
+
+function minhaFuncao(){
+
+}
+
+var d=1;
+var e=2;
+var f=d+e;
+console.log(f);

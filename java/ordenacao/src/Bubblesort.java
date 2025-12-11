fbf6b4f7db001af8990a0c2480c7295900380e4e
Matheus Mechetti Lulli <matheus.lulli@aluno.cefetmg.br>
2025-12-11 18:28:36 -0300

    Create Bubblesort.java

---
+++ java/ordenacao/src/Bubblesort.java
@@ -0,0 +1,28 @@
+public class Bubblesort {
+
+    //O bubble sort precisa de dois loops pra funcionar
+
+    public static void bubblesort(int[] v){
+        boolean houveTroca;
+        int aux;
+       
+       for(int b=0; b<v.length;b++){
+        houveTroca = false;
+        for(int i=0;i<v.length-1;i++){
+            if (v[i]>v[i+1]){
+                aux = v[i];
+                v[i]=v[i+1];
+                v[i+1]=aux;
+                houveTroca = true;          
+        }
+        }
+        
+        if(!houveTroca){
+            break;
+        }
+
+       }
+        
+    }
+    
+}

7b5d49b5cf7a1066865c9bc9cd46c277754424f1
Matheus Mechetti Lulli <matheus.lulli@aluno.cefetmg.br>
2025-12-11 18:47:19 -0300

    Update Selectionsort.java

--- java/ordenacao/src/Selectionsort.java
+++ java/ordenacao/src/Selectionsort.java
@@ -1,3 +1,25 @@
 public class Selectionsort {
-    //em cada loop, selecionar o menor item da parte desordenada e mover para a parte ordenada    
+    //em cada loop, selecionar o menor item da parte desordenada e mover para a parte ordenada
+    //olhar tudo, escolher um
+
+    //dois loops
+    
+    public static void selectionSort(int[] v){
+        int aux;
+        for (int i = 0; i<v.length-1;i++){
+            int min = i;
+            
+            for (int j=i+1;j<v.length;j++){
+                if(v[j]<v[min]){
+                min = j;
+            }
+            }
+            if (min !=i){
+                aux = v[i];
+                v[i] = v[min];
+                v[min]=aux;
+
+            }
+        }
+    }
 }

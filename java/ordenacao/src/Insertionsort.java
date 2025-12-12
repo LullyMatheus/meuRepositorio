146a6d0e1faeb6288a114511751f7439e0a2da44
Matheus Mechetti Lulli <matheus.lulli@aluno.cefetmg.br>
2025-12-12 05:46:01 -0300

    Update Insertionsort.java

--- java/ordenacao/src/Insertionsort.java
+++ java/ordenacao/src/Insertionsort.java
@@ -1,3 +1,24 @@
 public class Insertionsort {
+
+    public void insertionsort(int[] v){
+        // Começamos do índice 1 (o segundo elemento).
+        // Por quê? Porque assumimos que o elemento 0 (o primeiro) já está "ordenado" sozinho.
+        for (int i=1;i<v.length;i++){
+
+            int chave = v[i];
+
+            int j = i-1;
+
+            while(j>=0 && v[j]>chave){
+                v[j+1]=v[j];
+                j--;
+
+            }
+
+            //insertion
+            v[j+1]=chave;
+
+        }
+    }
     
 }
